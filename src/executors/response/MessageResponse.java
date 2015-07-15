b6ccbf1e45e1b3568382232e44dd341d39ed6dce
Gochi <germannparisi@gmail.com>
2015-07-15 15:22:21 -0300

    Agrego metodo addIdGroup y getIdGroup en MessageResponse.

--- src/executors/response/MessageResponse.java
+++ src/executors/response/MessageResponse.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 
 /**
  *
- * @author Parisi Germán & Bertola Federico
- * @version 1.0
+ * @author Parisi Germán y Bertola Federico
+ * @version 1.1
  */
 public class MessageResponse extends Response<String>{
     public MessageResponse(){
@@ -17,8 +17,16 @@ public class MessageResponse extends Response<String>{
         super.addValue("id", id);
     }
     
+    public void addIdGroup(String id){
+        super.addValue("id_group", id);
+    }
+    
     public String getId(){
         return super.getValue("id");
+    }
+    
+    public String getIdGroup(){
+        return super.getValue("id_group");
     }
     
     public void addText(String text){
